c12ba0458ca85cf71f30d470714ae20c918baf9a
Henrik Nørgaard <menneskefernis@gmail.com>
2019-12-31 00:38:09 +0100

    made the build_tree method recursive so it builds the tree

--- controller.rb
+++ controller.rb
@@ -1,4 +1,4 @@
 require_relative 'tree'
 
 tree = Tree.new
-puts tree.build_tree.moves.map { |move| move.to_s }
+puts tree.build_tree.moves[0].moves[0].moves.map { |move| move.to_s }
--- tree.rb
+++ tree.rb
@@ -10,30 +10,26 @@ class Tree
     @start = Move.new(4, 4)
   end
 
-  def build_tree(node=start, counter=0, array=[])
-    return if node.nil?
-    #unless array.empty?
-    #  return if array.max > 6
-    #end
-    #array << counter
-    
+  def build_tree(node=start, counter=0)
+    return if counter > 6
+
     operators = [["+", "+"], ["-", "-"], ["-", "+"], ["+", "-"]]
-    move1 = 1
-    move2 = 2
+    x_move = 1
+    y_move = 2
 
     operators.each do |op|
       2.times do
-        x_position = start.x.send(op[0], move1)
-        y_position = start.y.send(op[1], move2)
+        x_position = node.x.send(op[0], x_move)
+        y_position = node.y.send(op[1], y_move)
         
         if (x_position <= board.columns && x_position > 0) &&
            (y_position <= board.rows && y_position > 0)
-          node.moves << build_tree(Move.new(x_position, y_position))
+          node.moves << build_tree(Move.new(x_position, y_position), counter + 1)
         end
         x_move, y_move = y_move, x_move
       end
     end
-    
+
     node
   end
 end
